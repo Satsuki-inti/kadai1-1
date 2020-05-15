4d5ab0e9ebcc9321b1a3a70cdb82488d44f95339
itomayuuko <itomayuuko@192.168.11.4>
2020-05-15 15:46:13 +0900

    test

---
+++ kadai1-1/src/Food.java
@@ -0,0 +1,12 @@
+
+public class Food {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("好きな食べ物：パン");
+		System.out.println("理由：食事にもなって、おやつにもなる。そしてどんな食材にも合うから。");
+
+	}
+
+}
